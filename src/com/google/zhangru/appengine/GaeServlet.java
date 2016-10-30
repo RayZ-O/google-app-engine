b8616fffffbb6b1dc09dab986bba190cdbfa7cb4
rui <rayzhang1011@gmail.com>
2016-10-30 04:16:56 -0700

    Change indentation to 4 spaces

--- src/com/google/zhangru/appengine/GaeServlet.java
+++ src/com/google/zhangru/appengine/GaeServlet.java
@@ -5,8 +5,8 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class GaeServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-	}
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, world");
+    }
 }
